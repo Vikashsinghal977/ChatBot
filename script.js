14afcc5d14044bc10712c290d044b9183f4f0796
Vikash <vikashsinghal509@gmail.com>
2023-09-30 16:03:44 +0530

    Add modified

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const chatInput=document.querySelector(".chat-input textarea");
+const chatInput=document.querySelector(".chat-input textarea"); 
 const sendChatBtn=document.querySelector(".chat-input span");
 const chatbox=document.querySelector(".chatbox");
-
+const chatbotToggler = document.querySelector(".chatbot-toggler");
 
 let userMessage;
+const API_KEY = "";
 
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
@@ -14,7 +15,23 @@ const createChatLi = (message, className) => {
 }
 
 const generateResponse = () => {
-    const API_URL = "";
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const requestOptions = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                  role: "system",
+                  content: userMessage
+                }]
+        })
+    }
     
 }
 
@@ -23,10 +40,21 @@ const handleChat = () => {
     if(!userMessage) return;
     //append
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
     setTimeout(() => {
         chatbox.appendChild(createChatLi("Thinking...", "incoming"));
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse();
     }, 600);
+    chatInput.value="";
 }
 
+const show = () => {
+    var bute = document.getElementsByClassName("show-chatbot");
+    console.log(9);
+    bute.classList.toggle("show-chatbot");
+}
 
 sendChatBtn.addEventListener("click", handleChat);
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
